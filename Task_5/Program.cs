1337d3cbcb0fb8a93da251c008211b86f4207151
Denis <damned_wolf@mail.ru>
2023-02-11 20:49:39 +0300

    Task 5

---
+++ Task_5/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+int[] array = GetRandomArray();
+PrintResult();
+int[] GetRandomArray()
+{    
+    int a = new Random().Next(2, 15);
+    int[] result = new int[a];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1, 100);
+    }
+    return result;
+}
+int Diff(int[] array)
+{    
+    int diff = 0;
+    int min = array[0];
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i]) min = array[i];
+        if (max < array[i]) max = array[i];
+    }
+    diff = max - min;
+    return diff;
+}
+void PrintResult()
+{
+    Console.WriteLine($"Разница между максимальныйм и минимальным элементами массива: {Diff(array)}");
+    Console.Write($"[{String.Join(", ", array)}]");
+}
