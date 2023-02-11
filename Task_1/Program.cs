9232363a7430ff7453d72777957798c4e9908893
Denis <damned_wolf@mail.ru>
2023-02-11 16:48:56 +0300

    Task 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int numbersOfArray = int.Parse(Console.ReadLine());
+Console.Write("Введите диапазон элементов массива через пробел: ");
+string range = Console.ReadLine();
+int[] array = GetArray(numbersOfArray, range);
+Console.WriteLine($"Количество элементов массива лежащих в промежутке от 10 до 99 равно: {Result(array, 10, 99)}");
+PrintArray(array);
+int[] GetArray(int numbersOfArray, string range)
+{
+    int[] array = new int[numbersOfArray];    
+    string[] rangeNum = range.Split(" ", StringSplitOptions.RemoveEmptyEntries);    
+    int leftLimit = int.Parse(rangeNum[0]);
+    int rightLimit = int.Parse(rangeNum[1]);        
+    for (int j = 0; j < array.Length; j++)
+    {
+        array[j] = new Random().Next(leftLimit, rightLimit + 1);
+    }
+    return array;
+}
+int Result(int[] array, int minRange, int maxRange)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if(el >= minRange && el <= maxRange) count+=1;
+    }
+    return count;
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
